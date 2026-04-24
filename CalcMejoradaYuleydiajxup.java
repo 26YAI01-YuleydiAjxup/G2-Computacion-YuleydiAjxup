a6a3c89ad3c3f64b6a926d625cbeedd62c01361b
26YAI01-YuleydiAjxup <ajxupyuleydi15db@gmail.com>
2026-04-23 23:08:24 -0600

    Add files via upload

---
+++ CalcMejoradaYuleydiajxup.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcmejoradayuleydiajxup;
+
+/**
+ *
+ * @author yuley
+ */
+public class CalcMejoradaYuleydiajxup {
+
+    public static void main(String[] args) {
+        jfrmCalc holaYul=new jfrmCalc();
+        holaYul.setVisible(true);
+    }
+}
